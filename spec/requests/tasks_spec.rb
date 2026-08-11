3b1df8a2cb8eff1d7b7cab847615978c7d39e684
takaQ-hash <254258186+takaQ-hash@users.noreply.github.com>
2026-08-11 18:24:50 +0900

    ログイン・ログアウト機能の実装（Devise）（Issue #10） (#52)
    Deviseで自動生成されたファイル・設定を利用し、ログイン・ログアウト機能を実装しました。

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+    context "ログインしていない場合" do
+      it "ログイン画面にリダイレクトされる" do
+        get "/"
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
