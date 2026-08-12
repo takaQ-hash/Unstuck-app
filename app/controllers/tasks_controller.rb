93268a0466e60824718cf636af7c91269df0931e
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-08-12 16:55:57 +0900

    feat:タスク編集用のルーティングとコントローラーを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.includes(:user)
+    @tasks = Task.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -17,6 +17,21 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを更新しました"
+    else
+      flash.now[:danger] = "タスクの更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
+  
+  end
+  
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :tasks, only: %i[index new create]
+  resources :tasks, only: %i[index new create edit update]
 
   # Defines the root path route ("/")
   root "tasks#index"
