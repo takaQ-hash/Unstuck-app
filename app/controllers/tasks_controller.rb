abd78866dc864eb02a0cfc63d2600470ffd95fcd
takaQ-hash <254258186+takaQ-hash@users.noreply.github.com>
2026-08-12 17:08:29 +0900

    タスク編集機能の実装（Issue #13）  (#57)
    タスクの編集機能を実装しました。また、登録フォームをパーシャル化し、新規登録・編集画面に適用しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.includes(:user)
+    @tasks = Task.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -14,6 +14,20 @@ class TasksController < ApplicationController
     else
       flash.now[:danger] = "タスクの登録に失敗しました"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを更新しました"
+    else
+      flash.now[:danger] = "タスクの更新に失敗しました"
+      render :edit, status: :unprocessable_entity
     end
   end
 
