9d6fbb6a0687559802f3be3ccb4cec3bbec8cdea
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-08-12 13:06:30 +0900

    feat: タスク登録用のルーティングとコントローラーを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.includes(:user)
   end
+
+  def new
+    @task = Task.new(notification_type: :interval)
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました"
+    else
+      flash.now[:danger] = "タスクの登録に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+     params.require(:task).permit(:name, :deadline, :notification_type, :notification_value)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :tasks, only: %i[index]
+  resources :tasks, only: %i[index new create]
 
   # Defines the root path route ("/")
   root "tasks#index"
