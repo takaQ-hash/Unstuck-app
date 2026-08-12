6545027bbf3951652d3e714fd23b948df42fa6c4
takaQ-hash <254258186+takaQ-hash@users.noreply.github.com>
2026-08-12 18:00:56 +0900

    feat:タスク削除用のルーティングとコントローラーを追加 (#58)
    タスクの削除機能のうち、削除処理（ルーティング・コントローラー）を実装しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    task =  current_user.tasks.find(params[:id])
+    task.destroy!
+    redirect_to tasks_path,  notice: "タスクを削除しました"
+  end
+
   private
 
   def task_params
