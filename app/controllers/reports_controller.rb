edb4eae8bb0aad7e8df31dea8d44b08c891473bd
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-08-13 11:15:57 +0900

    fix:back_pathのXSS脆弱性を修正し、Brakemanを最新化

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,10 @@
+# app/controllers/reports_controller.rb
 class ReportsController < ApplicationController
   before_action :set_task
 
   def new
     @report = @task.reports.new(status: :in_progress)
-    @back_path = request.referer || task_path(@task)
+    @back_path = safe_back_path(request.referer) || task_path(@task)
   end
 
   def create
@@ -12,7 +13,7 @@ class ReportsController < ApplicationController
       redirect_to task_path(@task), notice: "報告が完了しました"
     else
       flash.now[:danger] = "報告が失敗しました"
-      @back_path = params[:back_path] || task_path(@task)
+      @back_path = safe_back_path(params[:back_path]) || task_path(@task)
       render :new, status: :unprocessable_entity
     end
   end
@@ -25,5 +26,20 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:status, :memo)
+  end
+
+  def safe_back_path(url)
+    return nil if url.blank?
+
+    begin
+      uri = URI.parse(url)
+    rescue URI::InvalidURIError
+      return nil
+    end
+
+    # 相対パス、または自サイト内のURLのみ許可
+    return nil unless uri.host.nil? || uri.host == request.host
+
+    uri.path.presence
   end
 end
