d8aa85d1cd8a0a9f4aa3106de7d7f2987a785d11
takaQ-hash <254258186+takaQ-hash@users.noreply.github.com>
2026-08-13 11:24:24 +0900

    報告登録機能の実装（Issue #16） (#61)
    タスクに対する報告を新規登録する機能を実装しました。

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,45 @@
+# app/controllers/reports_controller.rb
+class ReportsController < ApplicationController
+  before_action :set_task
+
+  def new
+    @report = @task.reports.new(status: :in_progress)
+    @back_path = safe_back_path(request.referer) || task_path(@task)
+  end
+
+  def create
+    @report = @task.reports.build(report_params)
+    if @report.save
+      redirect_to task_path(@task), notice: "報告が完了しました"
+    else
+      flash.now[:danger] = "報告が失敗しました"
+      @back_path = safe_back_path(params[:back_path]) || task_path(@task)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:task_id])
+  end
+
+  def report_params
+    params.require(:report).permit(:status, :memo)
+  end
+
+  def safe_back_path(url)
+    return nil if url.blank?
+
+    begin
+      uri = URI.parse(url)
+    rescue URI::InvalidURIError
+      return nil
+    end
+
+    # 相対パス、または自サイト内のURLのみ許可
+    return nil unless uri.host.nil? || uri.host == request.host
+
+    uri.path.presence
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_09_233355) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_12_135649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reports", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.integer "status", null: false
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_reports_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -38,5 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_09_233355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reports", "tasks"
   add_foreign_key "tasks", "users"
 end
