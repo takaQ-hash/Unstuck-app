e36fbfe309a2ea9675db02687c7f2fc759816752
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-08-18 22:46:23 +0900

    feat:User、Task、Reportのfactoryを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    encrypted_password { "MyString" }
+    sequence(:name) { |n| "user_#{n}" } 
+    sequence(:email) { |n| "test#{n}@sample.com" } 
+    password { "MyString" }
   end
 end
