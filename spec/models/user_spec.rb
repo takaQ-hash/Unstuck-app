dac71fa29bcdfe51a4cdf829b97dca5aab36d3ea
takaQ-hash <254258186+takaQ-hash@users.noreply.github.com>
2026-08-21 22:52:00 +0900

    10. ユーザーモデルのテスト追加（Issue #23） (#92)
    Userモデルに対するテストコードを追加しました。あわせて、テスト実装の過程で発覚したnameカラムの必須バリデーション漏れを修正しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '名前、メールアドレス、パスワードが全項目が正しく入力されている場合' do
+    it '有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  context '名前が存在しない場合' do
+    it '無効であること' do
+      user = build(:user, name: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+  end
+
+  context 'メールアドレスが存在しない場合' do
+    it '無効であること' do
+      user = build(:user, email: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+  end
+
+  context 'パスワードが存在しない場合' do
+    it '無効であること' do
+      user = build(:user, password: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:password]).to include('を入力してください')
+    end
+  end
+
+  context 'パスワードが6文字未満の場合' do
+    it '無効であること' do
+      user = build(:user, password: 'a' * 5)
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'メールアドレスに"@"がない場合' do
+    it '無効であること' do
+      user = build(:user, email: 'aaa.com')
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'メールアドレスがユニークでない場合' do
+    it '無効であること' do
+      user1 = create(:user)
+      user2 = build(:user)
+      user2.email = user1.email
+      user2.valid?
+      expect(user2.errors[:email]).to include('はすでに存在します')
+    end
+  end
+
+  context 'ユーザーが削除された場合' do
+    it '削除されたユーザーに紐づくタスクが削除されること' do
+      user = create(:user)
+      create(:task, user: user)
+      expect { user.destroy! }.to change(Task, :count).by(-1)
+    end
+  end
 end
