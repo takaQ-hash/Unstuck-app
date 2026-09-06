f47fd5219e514dc7bceb2ab0d3c0e5e5554cc755
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-09-06 10:24:11 +0900

    feat: バックグラウンドスケジューラー用のsend_pushエンドポイントを追加し、トークン認証を実装

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :push_subscriptions, only: %i[create]
 
   get "notifications/due_tasks", to: "notifications#due_tasks"
+  post "notifications/send_push", to: "notifications#send_push"
   get "guide", to: "guide#index"
   get "terms", to: "pages#terms"
   get "privacy", to: "pages#privacy"
