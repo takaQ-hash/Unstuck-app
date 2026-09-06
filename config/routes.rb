113ac9dd61bf5e3554c9f04b2f4147be51d9b34d
takaQ-hash <takaQ-hash@users.noreply.github.com>
2026-09-06 22:41:23 +0900

    fix: アプリ内通知を廃止し、Web Push通知に一本化する

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
 
   resources :push_subscriptions, only: %i[create]
 
-  get "notifications/due_tasks", to: "notifications#due_tasks"
   post "notifications/send_push", to: "notifications#send_push"
   get "guide", to: "guide#index"
   get "terms", to: "pages#terms"
